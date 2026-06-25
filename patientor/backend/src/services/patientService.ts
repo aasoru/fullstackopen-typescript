c9490f5deec1eadb03040bb0ac6d875029e24944
Aarón Sosa <aasoru@gmail.com>
2026-06-25 20:45:55 +0100

    exercise 14

--- patientor/backend/src/services/patientService.ts
+++ patientor/backend/src/services/patientService.ts
@@ -1,5 +1,6 @@
+import { v1 as uuid } from "uuid";
 import patients from "../../data/patients.ts";
-import type { NonSensitivePatient } from "../types.ts";
+import type { Patient, NonSensitivePatient, NewPatient } from "../types.ts";
 
 const getNonSensitiveEntries = (): NonSensitivePatient[] => {
   return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
@@ -11,4 +12,14 @@ const getNonSensitiveEntries = (): NonSensitivePatient[] => {
   }));
 };
 
-export default { getNonSensitiveEntries };
+const addPatient = (entry: NewPatient): Patient => {
+  const newPatient = {
+    id: uuid(),
+    ...entry,
+  };
+
+  patients.push(newPatient);
+  return newPatient;
+};
+
+export default { getNonSensitiveEntries, addPatient };
