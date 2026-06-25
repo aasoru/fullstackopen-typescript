476dedf40177f60fee72f916f3f411a355068524
Aarón Sosa <aasoru@gmail.com>
2026-06-25 20:45:55 +0100

    exercise 13

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express, { type Response } from "express";
 import patientService from "../services/patientService.ts";
 import type { NonSensitivePatient } from "../types.ts";
@@ -6,6 +7,18 @@ const router = express.Router();
 
 router.get("/", (_req, res: Response<NonSensitivePatient[]>) => {
   res.send(patientService.getNonSensitiveEntries());
+});
+
+router.post("/", (req, res) => {
+  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+  const newPatient = patientService.addPatient({
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation,
+  });
+  res.json(newPatient);
 });
 
 export default router;
