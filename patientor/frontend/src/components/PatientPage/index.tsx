230ff8982be88f9f512febe1659bc10b505c2d69
Aarón Sosa <aasoru@gmail.com>
2026-07-05 20:21:51 +0100

    exercise 24

---
+++ patientor/frontend/src/components/PatientPage/index.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Typography } from "@mui/material";
+import FemaleIcon from "@mui/icons-material/Female";
+import MaleIcon from "@mui/icons-material/Male";
+import TransgenderIcon from "@mui/icons-material/Transgender";
+
+import patientService from "../../services/patients";
+import { Patient, Gender } from "../../types";
+
+const PatientPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const [patient, setPatient] = useState<Patient | null>(null);
+
+  useEffect(() => {
+    if (id) {
+      void patientService.getById(id).then(setPatient);
+    }
+  }, [id]);
+
+  if (!patient) return null;
+
+  const genderIcon = () => {
+    switch (patient.gender) {
+      case Gender.Female:
+        return <FemaleIcon />;
+      case Gender.Male:
+        return <MaleIcon />;
+      default:
+        return <TransgenderIcon />;
+    }
+  };
+
+  return (
+    <div>
+      <Typography variant="h4">
+        {patient.name} {genderIcon()}
+      </Typography>
+      <p>ssn: {patient.ssn}</p>
+      <p>occupation: {patient.occupation}</p>
+      <p>date of birth: {patient.dateOfBirth}</p>
+    </div>
+  );
+};
+
+export default PatientPage;
