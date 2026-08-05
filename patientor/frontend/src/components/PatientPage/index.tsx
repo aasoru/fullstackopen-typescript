f40fab4745806a4ef6305af244be654fe9579fd4
Aarón Sosa <aasoru@gmail.com>
2026-08-05 19:19:43 +0100

    exercise 26

--- patientor/frontend/src/components/PatientPage/index.tsx
+++ patientor/frontend/src/components/PatientPage/index.tsx
@@ -39,6 +39,19 @@ const PatientPage = () => {
       <p>ssn: {patient.ssn}</p>
       <p>occupation: {patient.occupation}</p>
       <p>date of birth: {patient.dateOfBirth}</p>
+      <Typography variant="h5">entries</Typography>
+      {patient.entries.map((entry) => (
+        <div key={entry.id}>
+          <p>
+            {entry.date} {entry.description}
+          </p>
+          <ul>
+            {entry.diagnosisCodes?.map((code) => (
+              <li key={code}>{code}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
